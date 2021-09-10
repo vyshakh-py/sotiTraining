c16a38cded99dc288dfb946e0d2f0b1daaff2ccc
vyshakh-py <65709781+vyshakh-py@users.noreply.github.com>
2021-09-10 03:42:03 -0400

    Add files via upload

---
+++ calc/calc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc();
+        }
+
+        static void Calc()
+        {
+            Console.Write("enter no.:");
+            int x=Convert.ToInt32(Console.ReadLine());
+
+      
+
+            Console.Write("enter no.:");
+            int y= Convert.ToInt32(Console.ReadLine());
+        
+
+            Console.WriteLine( x+y);
+
+            Console.ReadLine();
+        }
+    }
+}
