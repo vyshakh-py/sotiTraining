0498a28f76e059901b197c283fae615dfd27e792
vyshakh-py <65709781+vyshakh-py@users.noreply.github.com>
2021-09-15 08:59:37 +0530

    Update User.cs

--- HMS/HMS/User.cs
+++ HMS/HMS/User.cs
@@ -1,12 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HMS
+namespace assessment1
 {
-    class Class1
+    
+    class User
     {
+        string Title;
+        string FirstName { get; set; }
+        string Email { get; set; }
+        string DOB { get; set; }
+
+        public User(string title, string name, string email, string dob)
+        {
+            this.FirstName = name;
+            this.Title = title;
+            this.Email = email;
+            this.DOB = dob;
+            
+        }
+        public void changePW()
+        {
+
+        }
+       
+    }
+
+    enum Role
+    {
+        Physician, Nurse, Admin
+    }
+
+    class Patient : User
+    {
+        public double ContactNumber { get; set; }
+        private string password { get; set; }
+
+        
     }
 }
